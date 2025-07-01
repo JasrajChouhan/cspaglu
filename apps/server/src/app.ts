0021ed321b91a14e098adc6c50c7ecbf6a6dcadf
JasrajChouhan <jasrajc945@gmail.com>
2025-07-02 02:48:51 +0530

    chore(server): create http server

---
+++ apps/server/src/app.ts
@@ -0,0 +1,30 @@
+import express, { Response, type Express } from 'express';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import cors from 'cors';
+import ip from 'ip';
+
+const app: Express = express();
+
+// ============= Middleware Layer =============
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Routes
+
+app.get('/ping', (_req, res : Response) => {
+	res.json({
+		success: true,
+		message: 'Pong',
+		ipAddress: ip.address()
+	});
+});
+
+// 404 handler
+// Global error handler
+
+export default app;
