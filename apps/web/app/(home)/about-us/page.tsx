bc6bbb750b7826ace9262d5f1cba442d095b47d8
JasrajChouhan <jasrajc945@gmail.com>
2025-07-03 13:28:27 +0530

    feat(ui): add hero seciton for about us page

---
+++ apps/web/app/(home)/about-us/page.tsx
@@ -0,0 +1,9 @@
+import HeroSectionAboutUs from "./_components/hero-section-about-us";
+
+export default function AboutUs() {
+  return (
+    <div className="dark:border-x dark:border-b border-dashed border-x-2 border-b-2 bg-background text-foreground">
+      <HeroSectionAboutUs />
+    </div>
+  );
+}
