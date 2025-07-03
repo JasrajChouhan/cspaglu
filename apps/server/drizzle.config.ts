be547e3f4f9893e25f1057c60c892ccb635235be
JasrajChouhan <jasrajc945@gmail.com>
2025-07-03 20:07:07 +0530

    feat: add env config related to db in drizzle

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -1,7 +1,23 @@
 import { type Config } from "drizzle-kit";
-import "dotenv/config";
-import dotenv from "dotenv";
-dotenv.config();
+
+import * as path from "path";
+import * as dotenv from "dotenv";
+import * as fs from "fs";
+
+const env = process.env.NODE_ENV || "local";
+const envFile = `.env.${env}`;
+const envPath = path.resolve(__dirname, envFile);
+
+if (!fs.existsSync(envPath)) {
+  throw new Error(`Environment file "${envFile}" not found.`);
+}
+
+dotenv.config({ path: envPath });
+
+if (!process.env.DATABASE_URL) {
+  throw new Error(`DATABASE_URL not set in "${envFile}".`);
+}
+
 
 export default {
   schema: "./src/db/schema.ts",
