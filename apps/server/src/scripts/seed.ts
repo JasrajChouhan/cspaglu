41190ae4fdc9fc86948c5af91e07bd125397797a
JasrajChouhan <jasrajc945@gmail.com>
2025-07-04 23:49:04 +0530

    chore(db): add seed script for waitlist end

---
+++ apps/server/src/scripts/seed.ts
@@ -0,0 +1,22 @@
+// apps/server/scripts/seed.ts
+
+import { db } from "../utils/db";
+import { featureFlags} from '../db/schema';
+
+async function seed() {
+  try {
+    await db.insert(featureFlags).values([
+      {
+        key: "waitlistEnded",
+        value: false,
+      },
+    ]);
+    console.log("Seed completed.");
+  } catch (error) {
+    console.error("Seed failed:", error);
+  }finally {
+    process.exit(1);
+  }
+}
+
+seed();
